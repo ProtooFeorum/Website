b3e3ba2f220f620cfd7dea26e447ab0a445b3f61
Feodor Salauyou <protoofeorum@gmail.com>
2022-02-16 23:56:52 +0300

    иутуашеы 2 gfgrf

---
+++ Benefits/Benefits_Number_2/Befenits_Number_2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Befenits_Number_2-style.scss">
+    <title>Benefits</title>
+</head>
+<body>
+    <div class="main">
+        <div class="main__content">
+            <div class="content__body">
+                <div class="content__title">
+                     Our Features
+                </div>
+                <div class="content__subtitle">
+                     We work hard every day to make life of our clients better and happier
+                </div>
+                 <div class="content__benefits">
+                     <div class="benefits-item">
+                        <div class="item__icon"><img src="img/icon.png" alt=""></div>
+                        <div class="item__info">
+                          <div class="item__title">Hight Quality</div>
+                          <div class="item__text"><p>We are a leading firm in providing quality and value to our customers. Each member of our team has at least 5 years of legal experience.</p></div>
+                        </div>
+                    </div>
+                     <div class="benefits-item">
+                        <div class="item__icon"><img src="img/icon.png" alt=""></div>
+                        <div class="item__info">
+                          <div class="item__title">Good Support</div>
+                          <div class="item__text"><p>Our managers are always ready to answer your questions. You can call us at the weekends and at night. You can also visit our office for a personal consultation.</p></div>
+                        </div>
+                    </div>
+                     <div class="benefits-item">
+                        <div class="item__icon"><img src="img/icon.png" alt=""></div>
+                        <div class="item__info">
+                          <div class="item__title">Individual Approach</div>
+                          <div class="item__text"><p>Our company works according to the principle of individual approach to every client. This method allows us to achieve success in problems of all levels.</p></div>
+                        </div>
+                    </div>
+                 </div>             
+            </div>
+        </div>
+    </div>
+</body>    
